eab7c91dd3ac0560e9cd9f5c3612170ea693e9a4
jeong-hae-rim <usemono33@gmail.com>
2024-08-08 00:18:06 +0900

    feat: 글귀 수정

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -14,9 +14,10 @@ function App() {
       <div onClick={handleOnClick}>
         <img src={reactLogo} className="logo" alt="React logo" />
       </div>
-      <h1 className="title">호열X대만 온리전</h1>
+      <h2 className="title">호열X대만 온리전</h2>
       <h1 className="title">LOVE PUNCH</h1>
       <p className="read-the-docs">2024. 8. 10</p>
+      <p className="read-the-docs">SLAM DUNK NON-OFFICIAL FESTIVAL</p>
     </>
   );
 }
--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -14,7 +14,8 @@ import {
 function Info() {
   return (
     <div>
-      <h1 className="title">LOVE PUNCH INFO</h1>
+      <h1 className="title">LOVE PUNCH</h1>
+      <h2 className="title">통합 부스 & 인포</h2>
       <div className="cards">
         <div className="cards__container">
           <div className="cards__wrapper">
