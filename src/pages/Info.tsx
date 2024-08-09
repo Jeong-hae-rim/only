7033c1f1e2b11e2843349af128ffed3a14266727
jeong-hae-rim <usemono33@gmail.com>
2024-08-09 23:34:36 +0900

    feat: 부스 인포 업데이트 및 notice 추가

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -26,6 +26,28 @@ function Info() {
         searchTerm={searchTerm}
         onSearchChange={handleSearchChange}
       />
+      <div className="notice__container">
+        <h3 className="title">NOTICE</h3>
+        <div className="notice__1">
+          <p className="notice__title">
+            모든 부스 내 선입금이 이미 진행된 품목은 <br />
+            <span>당일 현장에 수량이 적거나 없을 수</span> 있습니다.
+          </p>
+          <p className="notice__title">
+            꼭 작가님에게 여쭤보고 구매를 진행해 주세요.
+          </p>
+        </div>
+        <div className="notice__2">
+          <p className="notice__title">
+            8월 10일 온리전 당일은 <br /> 옆 홀에서{" "}
+            <span>정기 시험이 진행</span> 중입니다.
+          </p>
+          <p className="notice__title">
+            정숙한 자세로 현판 사냥을 하는 <br /> 멋진 호댐러의 모습을
+            보여줍시다.
+          </p>
+        </div>
+      </div>
       <Card infoData={filteredData} />
     </div>
   );
