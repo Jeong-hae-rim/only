2a67f3367848d86b19260957079949db97e566c8
jeong-hae-rim <usemono33@gmail.com>
2024-08-10 23:16:19 +0900

    feat: 정리

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,31 +1,26 @@
-import "./info.css";
-import { useState } from "react";
-import { data } from "../data/data";
-import Card from "../components/Card";
-import SearchInput from "../components/Search";
+// import "./info.css";
+// import { useState } from "react";
+// import { data } from "../data/data";
+// import Card from "../components/Card";
+// import SearchInput from "../components/Search";
 
 function Info() {
-  const [searchTerm, setSearchTerm] = useState<string>("");
+  // const [searchTerm, setSearchTerm] = useState<string>("");
 
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-  };
+  // const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setSearchTerm(e.target.value);
+  // };
 
-  const filteredData = data.filter(
-    (item) =>
-      item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      item.author.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      item.label.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  // const filteredData = data.filter(
+  //   (item) =>
+  //     item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+  //     item.author.toLowerCase().includes(searchTerm.toLowerCase()) ||
+  //     item.label.toLowerCase().includes(searchTerm.toLowerCase())
+  // );
 
   return (
     <div className="info__container">
       <h1 className="title">LOVE PUNCH</h1>
-      {/* <h2 className="title">통합 부스 & 인포</h2> */}
-      {/* <SearchInput
-        searchTerm={searchTerm}
-        onSearchChange={handleSearchChange}
-      /> */}
       <div className="notice__container">
         <h3 className="title">LETTER</h3>
         <div className="notice__1">
@@ -87,7 +82,6 @@ function Info() {
           </p>
         </div>
       </div>
-      {/* <Card infoData={filteredData} /> */}
     </div>
   );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
-// import { Header } from "./components";
 import Info from "./pages/Info";
-import InfoDetail from "./pages/InfoDetail";
 import NotFound from "./pages/404";
 
 const Router = () => {
@@ -12,7 +10,6 @@ const Router = () => {
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/love-you" element={<Info />} />
-          {/* <Route path="/info/:id" element={<InfoDetail />} /> */}
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
