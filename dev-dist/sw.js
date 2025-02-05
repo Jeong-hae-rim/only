f66bc6e355d4447cec0252bda4001e9569a3684e
jeong-hae-rim <usemono33@gmail.com>
2025-02-06 02:02:37 +0900

    feat: 서치 바 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nai9ho5raco"
+    "revision": "0.d7tl7nqvu6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -13,13 +13,23 @@ const SearchInput: React.FC<SearchInputProps> = ({
     <div className="search-container">
       <div className="search-8bit">
         <div className="search-wrapper">
+          {/* 숨겨진 input */}
           <input
             type="text"
             value={searchTerm}
             onChange={onSearchChange}
             placeholder="부스 명 / 작가님 이름 / 부스 위치로 검색"
-            className="search-input"
+            className="hidden-input"
           />
+          {/* 커스텀 스타일 적용된 div */}
+          <div
+            className="search-input"
+            onClick={() =>
+              document.querySelector<HTMLInputElement>(".hidden-input")?.focus()
+            }
+          >
+            {searchTerm || "부스 명 / 작가님 이름 / 부스 위치로 검색"}
+          </div>
         </div>
       </div>
     </div>
