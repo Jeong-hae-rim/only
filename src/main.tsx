d1cb79f5957fa766715ab0b41a29d2ad88d92c26
jeong-hae-rim <usemono33@gmail.com>
2025-03-06 23:35:25 +0900

    feat: PWA Update 위치 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,4 @@
 import ReactDOM from "react-dom/client";
 import Router from "./routes.tsx";
-import PWAUpdate from "./func/PWAUdate.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
-    <PWAUpdate />
-    <Router />
-  </>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<Router />);
